1a077e953aad801b44c554e327faeba10951e0ca
n1ckfg <nick@fox-gieg.com>
2018-07-20 13:07:36 -0400

    begin threaded example

---
+++ basic/radio-buttons.py
@@ -0,0 +1,42 @@
+#import tkinter as tk # Python 3
+import Tkinter as tk # Python 2
+
+def openfile():
+	print("EEEEEE")
+
+def closefile():
+	print("AAAAAA")
+
+def zoomview():
+	print("OOOOOOO")
+
+buttonWidth = 20
+buttonPadX = 20
+buttonPadY = 10
+buttonColumnSpan = 2
+numButtons = 3
+windowPadX = 40
+windowPadY = 15
+windowWidth = 512
+windowHeight = 256
+
+window = tk.Tk()
+window.title("Commands")
+window.geometry(str(windowWidth) + "x" + str(windowHeight))
+canvas = tk.Canvas(window)
+canvas.grid(row=0, column=0, padx=windowPadX, pady=windowPadY)
+
+openButton = tk.Button(canvas, text="Open File", width=buttonWidth, command=openfile)
+openButton.grid(row=1, column=0, columnspan=buttonColumnSpan, padx=buttonPadX, pady=buttonPadY, sticky=tk.W)
+openButton.configure(background="white")
+
+closeButton = tk.Button(canvas, text="Close File", width=buttonWidth, command=closefile)
+closeButton.grid(row=2, column=0, columnspan=buttonColumnSpan, padx=buttonPadX, pady=buttonPadY, sticky=tk.W)
+closeButton.configure(background="white")
+
+zoomButton = tk.Button(canvas, text="Zoom View", width=buttonWidth, command=zoomview)
+zoomButton.grid(row=3, column=0, columnspan=buttonColumnSpan, padx=buttonPadX, pady=buttonPadY, sticky=tk.W)
+zoomButton.configure(background="white")
+
+tk.mainloop()
+
---
+++ threaded/radio-buttons2.py
@@ -0,0 +1,30 @@
+
+#import tkinter as tk # Python 3
+import Tkinter as tk # Python 2
+import threading
+
+# https://stackoverflow.com/questions/459083/how-do-you-run-your-own-code-alongside-tkinters-event-loop
+class App(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.start()
+
+    def callback(self):
+        self.root.quit()
+
+    def run(self):
+        self.root = tk.Tk()
+        self.root.protocol("WM_DELETE_WINDOW", self.callback)
+
+        label = tk.Label(self.root, text="Hello World")
+        label.pack()
+
+        self.root.mainloop()
+
+
+app = App()
+print('Now we can continue running code while mainloop runs!')
+
+for i in range(100000):
+    print(i)
